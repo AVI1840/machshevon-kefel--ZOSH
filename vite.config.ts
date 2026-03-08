3e747413966db09b8a02b66d4318074b8d65fb6d
aviad1840 <aviad1840@gmail.com>
2026-03-08 16:35:01 +0200

    fix: base path for aviad1840 GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/machshevon-kefel--ZOSH/',
+  base: '/machshevon-kefel/',
   server: {
     host: "::",
     port: 8080,
