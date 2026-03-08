873166718d34421b22a933d8d38b356fe63b9a51
aviad1840 <aviad1840@gmail.com>
2026-03-08 15:44:45 +0200

    fix: update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/machshevon-kefel/',
+  base: '/machshevon-kefel--ZOSH/',
   server: {
     host: "::",
     port: 8080,
